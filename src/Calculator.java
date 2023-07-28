ab56ee5e6e045de6bb09e8d1e1f3a8ae3e5cdd65
katherinemoore <katherinemoore789@gmail.com>
2023-07-27 18:58:12 -0700

    First commit

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+
+public class Calculator {
+	
+	public static int add(int x, int y) {
+		return x + y;
+	}
+	
+	public static int subtract(int x, int y) {
+		return x - y;
+	}
+	
+	public static int multiply(int x, int y) {
+		return x * y;
+	}
+	
+	public static int divide(int x, int y) {
+		return x / y;
+	}
+	
+	public static int square(int x) {
+		return x * x;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Calculator.add(2, 4); //returns 6
+		Calculator.subtract(4, 2); //returns 2
+		Calculator.multiply(2, 4); //returns 8
+		Calculator.divide(4, 2); //returns 2
+		Calculator.square(4); //returns 16
+
+	}
+
+}
